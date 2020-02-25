7034e73067ac7ea1acc75beebe523b92bba553cf
Gala <galinasian@gmail.com>
2020-02-25 18:20:56 +0100

    Subido ejercicio 2

---
+++ main.js
@@ -0,0 +1,13 @@
+/*Ejercicio 2: escribe un programa con cuatro variables de tipo number (a, b, c y d) y un condicional que imprima por pantalla si la suma de a y b es mayor que la suma de c y d. */
+const a = 2;
+const b = 4;
+const c = 6;
+const d = 8;
+if (a + b > c + d)  {
+  console.log("La suma de a + b es mayor que la suma de c + d")
+} else {
+  console.log('La suma de a y b es menor que c + d');
+  
+}
+
+
